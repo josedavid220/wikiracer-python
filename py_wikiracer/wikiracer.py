0257e1f5c25ffc080038a3cb29413f582be91d51
Jose Ocampo <joseocampo220@gmail.com>
2025-09-11 17:59:00 -0500

    feat: implement wikiracer method

--- py_wikiracer/wikiracer.py
+++ py_wikiracer/wikiracer.py
@@ -2,6 +2,8 @@ from py_wikiracer.internet import Internet
 from typing import List
 import re
 import heapq
+import Levenshtein
+
 
 class Parser:
 
@@ -47,10 +49,12 @@ class Parser:
 # In these methods, we are given a source page and a goal page, and we should return
 #  the shortest path between the two pages. Be careful! Wikipedia is very large.
 
+
 class DijkstrasProblem:
     def __init__(self):
         self.internet = Internet()
         self.parser = Parser()
+
     # Links should be inserted into the heap as they are located in the page.
     # By default, the cost of going to a link is the length of a particular destination link's name. For instance,
     #  if we consider /wiki/a -> /wiki/ab, then the default cost function will have a value of 8.
@@ -104,26 +108,88 @@ class DijkstrasProblem:
                 if source == goal:
                     path.append(goal)
                 return path
-        
+
         # If the loop finishes without finding the goal
         return None
+
 
 class WikiracerProblem:
     def __init__(self):
         self.internet = Internet()
+        self.parser = Parser()
 
-    # Time for you to have fun! Using what you know, try to efficiently find the shortest path between two wikipedia pages.
-    # Your only goal here is to minimize the total amount of pages downloaded from the Internet, as that is the dominating time-consuming action.
+    def costFn(self, current_link, neighbor_link, goal_links, goal):
+        MAX_PRIORITY_SCORE = 0
 
-    # Your answer doesn't have to be perfect by any means, but we want to see some creative ideas.
-    # One possible starting place is to get the links in `goal`, and then search for any of those from the source page, hoping that those pages lead back to goal.
+        # Check if the neighbor link is contained in the links
+        # of the goal, hoping there is a bidirectional connection
+        if neighbor_link in goal_links:
+            return MAX_PRIORITY_SCORE
 
-    # Note: a Dijkstra implementation with no optimizations will not get credit, and it will suck.
-    # You may find Internet.get_random() useful, or you may not.
+        # NOTE: This doesn't add value! It is a weaker version of the Levenshtein distance
+        # # Check if a neighbor link is a substring or a superstring of the goal link
+        # # Exclude neighbors that don't add information, like '/wiki/A'
+        # wikiless_goal = goal[6:]
+        # wikiless_neighbor = neighbor_link[6:]
+        # min_wiki_length = 2
+        # if len(wikiless_neighbor) > min_wiki_length and len(wikiless_goal) > min_wiki_length :
+        #     # goal_lower = goal.lower()
+        #     # neighbor_link_lower = neighbor_link.lower()
+        #     neighbor_is_substr = wikiless_goal.find(wikiless_neighbor) != -1
+        #     neighbor_is_supstr = wikiless_neighbor.find(wikiless_goal) != -1
 
-    def wikiracer(self, source = "/wiki/Calvin_Li", goal = "/wiki/Wikipedia"):
-        path = [source]
-        # YOUR CODE HERE
-        # ...
-        path.append(goal)
-        return path # if no path exists, return None
+        #     # Proritize neighbors with any of the previous properties
+        #     if neighbor_is_substr or neighbor_is_supstr:
+        #         return MAX_PRIORITY_SCORE
+
+        return Levenshtein.distance(neighbor_link, goal)
+
+    def wikiracer(self, source="/wiki/Calvin_Li", goal="/wiki/Wikipedia"):
+        path = []
+        distances = {source: 0}
+        priority_queue = [(0, source)]  # (distance, link)
+        goal_html = self.internet.get_page(goal)
+
+        goal_links = self.parser.get_links_in_page(goal_html)
+        self.internet.requests.clear()  # To prevent validation errors in tests
+
+        # Dictionary to store the predecessor of each link
+        predecessors = {source: None}
+
+        while priority_queue:
+            current_distance, current_link = heapq.heappop(priority_queue)
+
+            # If the extracted distance is greater than the known shortest, skip
+            if current_distance > distances[current_link]:
+                continue
+
+            current_link_html = self.internet.get_page(current_link)
+            neighbor_links = self.parser.get_links_in_page(current_link_html)
+
+            for neighbor_link in neighbor_links:
+                if neighbor_link not in distances:
+                    distances[neighbor_link] = float("infinity")
+                    predecessors[neighbor_link] = None
+
+                weight = self.costFn(current_link, neighbor_link, goal_links, goal)
+                distance = current_distance + weight
+
+                # If a shorter path to the neighbor is found, update and push to queue
+                if distance < distances[neighbor_link]:
+                    distances[neighbor_link] = distance
+                    predecessors[neighbor_link] = current_link
+                    heapq.heappush(priority_queue, (distance, neighbor_link))
+
+            # If we reached the target, reconstruct and return the path
+            if goal in neighbor_links:
+                neighbor_link = goal
+                while neighbor_link is not None:
+                    path.insert(0, neighbor_link)
+                    neighbor_link = predecessors[neighbor_link]
+
+                if source == goal:
+                    path.append(goal)
+                return path
+
+        # If the loop finishes without finding the goal
+        return None
